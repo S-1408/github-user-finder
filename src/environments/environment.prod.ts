f7f0c617abf7376148ca3f6bfc985584119576e9
Sonam Verma <60258353+S-1408@users.noreply.github.com>
2021-01-17 22:33:25 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig  : {
-    apiKey: 'AIzaSyCtRoF65O2q4MV3SIys5QuoIz6HgOE0NJw',
-    authDomain: 'angular-10-47090.firebaseapp.com',
-    projectId: 'angular-10-47090',
-    storageBucket: 'angular-10-47090.appspot.com',
-    messagingSenderId: '237779402978',
-    appId: '1:237779402978:web:930d2d89b485193a9e1ebb'
-  }
+  
 };
